f9ef1e13b6a13783505ebe9d2444fee7637ff3f4
Maxatefoe <2425404069@live.gctu.edu.gh>
2026-05-29 08:37:24 +0000

    Implement initials printing in initials.py
    Added a program to print initials and assignment details.

--- initials.py
+++ initials.py
@@ -2,6 +2,34 @@
 def main():
   pass #code goes here
 
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Solomon Adu Atefoe
+# STUDENT ID:  2425404069
+# Email: 2425404069@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Solomon Adu Atefoe, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+print("...SAA")
+print()
+print()
+print(".....SSSSSSSSS........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+print("....SSSSSSSSSS........AA........AA........AA........AA.....")
+print("...SSSS...............AA........AA........AA........AA.....")
+print("...SSSS...............AA........AA........AA........AA.....")
+print(".....SSSSSS...........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+print("......SSSSSS..........AA........AA........AA........AA.....")
+print("..........SSSS........AA........AA........AA........AA.....")
+print("..........SSSS........AA........AA........AA........AA.....")
+print("...SSSSSSSSSSS...**...AA........AA...**...AA........AA...**")
+print("...SSSSSSSSSS....**...AA........AA...**...AA........AA...**")
+
+
 
 
 main()
