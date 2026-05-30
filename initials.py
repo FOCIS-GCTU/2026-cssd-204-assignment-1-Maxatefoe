55b4ca754868e12f42fd5437fda8cbb4f4cda877
Maxatefoe <2425404069@live.gctu.edu.gh>
2026-05-30 17:05:48 +0000

    Implement initials printing functionality
    Updated the main function to print initials and a detailed representation of them.

--- initials.py
+++ initials.py
@@ -1,31 +1,31 @@
 
 def main():
-  pass #code goes here
+  
+  # File: initials.py
+  # Description: Print out my initials.
+  # Assignment Number: 1
+  #
+  # Name: Solomon Adu Atefoe
+  # STUDENT ID:  2425404069
+  # Email: 2425404069@live.gctu.edu.gh
+  # Grader: Mr. Augustus Buckman
+  #
+  # On my honor, Solomon Adu Atefoe, this programming assignment is my own work
+  # and I have not provided this code to any other student.
 
-# File: initials.py
-# Description: Print out my initials.
-# Assignment Number: 1
-#
-# Name: Solomon Adu Atefoe
-# STUDENT ID:  2425404069
-# Email: 2425404069@live.gctu.edu.gh
-# Grader: Mr. Augustus Buckman
-#
-# On my honor, Solomon Adu Atefoe, this programming assignment is my own work
-# and I have not provided this code to any other student.
+  print("...SAA")
+  print()
 
-print("...SAA")
-print()
+  print(".....SSSSSSSSS...........AAAAAA..............AAAAAA........")
+  print("....SSSSSSSSSS..........AA....AA............AA....AA.......")
+  print("...SSSS................AA......AA..........AA......AA......")
+  print("...SSSS...............AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+  print(".....SSSSSS...........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+  print("......SSSSSS..........AA........AA........AA........AA.....")
+  print("..........SSSS........AA........AA........AA........AA.....")
+  print("..........SSSS........AA........AA........AA........AA.....")
+  print("...SSSSSSSSSSS...**...AA........AA...**...AA........AA...**")
+  print("...SSSSSSSSSS....**...AA........AA...**...AA........AA...**")
 
-print(".....SSSSSSSSS........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
-print("....SSSSSSSSSS........AA........AA........AA........AA.....")
-print("...SSSS...............AA........AA........AA........AA.....")
-print("...SSSS...............AA........AA........AA........AA.....")
-print(".....SSSSSS...........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
-print("......SSSSSS..........AA........AA........AA........AA.....")
-print("..........SSSS........AA........AA........AA........AA.....")
-print("..........SSSS........AA........AA........AA........AA.....")
-print("...SSSSSSSSSSS...**...AA........AA...**...AA........AA...**")
-print("...SSSSSSSSSS....**...AA........AA...**...AA........AA...**")
 
 main()
