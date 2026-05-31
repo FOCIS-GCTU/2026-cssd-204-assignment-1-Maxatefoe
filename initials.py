280c3a2742241aef3d091a67ee6aabfe103f6b80
Maxatefoe <2425404069@live.gctu.edu.gh>
2026-05-31 00:05:49 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -14,6 +14,7 @@
 def main():
   # This is my first python class program(not my first python program), this program helps me get use to the print function by outputing the initials of my name...
 
+  
   print()
   print("...SAA")
   print()
